0f2bc20d625dd468b65001f6ad2177603533d72f
sdbills <>
2025-01-27 15:30:03 -0700

    RookMoves

--- shared/src/main/java/chess/movecalculators/RookMovesCalculator.java
+++ shared/src/main/java/chess/movecalculators/RookMovesCalculator.java
@@ -11,7 +11,25 @@ public class RookMovesCalculator extends PieceMovesCalculator{
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         var moves = new ArrayList<ChessMove>();
-
+        int[] directions = {-1,1};
+        for (var n : directions) {
+            for (var m : directions) {
+                int i = 1;
+                ChessPosition newPosition;
+                do {
+                    if (m == -1) {
+                        newPosition = new ChessPosition(position.getRow()+i*n, position.getColumn());
+                    } else {
+                        newPosition = new ChessPosition(position.getRow(), position.getColumn()+i*n);
+                    }
+                    var newMove = new ChessMove(position,newPosition);
+                    if (isLegalMove(board,newMove)) {
+                        moves.add(newMove);
+                    }
+                    i++;
+                } while (newPosition.isValid() && board.getPiece(newPosition) == null);
+            }
+        }
         return moves;
     }
 }
