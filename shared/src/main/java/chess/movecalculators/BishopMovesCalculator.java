43d7c6dc5f4825d21b70d0cfc8480ece6fc85e4f
sdbills <>
2025-01-27 15:29:59 -0700

    BishopMoves

--- shared/src/main/java/chess/movecalculators/BishopMovesCalculator.java
+++ shared/src/main/java/chess/movecalculators/BishopMovesCalculator.java
@@ -11,7 +11,21 @@ public class BishopMovesCalculator extends PieceMovesCalculator{
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         var moves = new ArrayList<ChessMove>();
-
+        int[] directions = {-1,1};
+        for (var n : directions) {
+            for (var m : directions) {
+                int i = 1;
+                ChessPosition newPosition;
+                do {
+                    newPosition = new ChessPosition(position.getRow()+i*n, position.getColumn()+i*m);
+                    var newMove = new ChessMove(position,newPosition);
+                    if (isLegalMove(board,newMove)) {
+                        moves.add(newMove);
+                    }
+                    i++;
+                } while (newPosition.isValid() && board.getPiece(newPosition) == null);
+            }
+        }
         return moves;
     }
 }
